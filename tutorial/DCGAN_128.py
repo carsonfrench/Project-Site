af9f336c8f99452072a843750a3dedd299ee5ffa
csaa2019 <78115222+csaa2019@users.noreply.github.com>
2022-09-21 14:07:59 -0700

    add save code

--- tutorial/DCGAN_128.py
+++ tutorial/DCGAN_128.py
@@ -334,37 +334,9 @@ for epoch in range(num_epochs):
 
 
 # %%
-
-
-plt.figure(figsize=(10,5))
-plt.title("Generator and Discriminator Loss During Training")
-plt.plot(G_losses,label="G")
-plt.plot(D_losses,label="D")
-plt.xlabel("iterations")
-plt.ylabel("Loss")
-plt.legend()
-plt.show()
-
+torch.save(model, 'new_model.pth')
 
 # %%
-
-
-# Grab a batch of real images from the dataloader
-real_batch = next(iter(dataloader))
-
-# Plot the real images
-plt.figure(figsize=(15,15))
-plt.subplot(1,2,1)
-plt.axis("off")
-plt.title("Real Images")
-plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
-
-# Plot the fake images from the last epoch
-plt.subplot(1,2,2)
-plt.axis("off")
-plt.title("Fake Images")
-plt.imshow(np.transpose(img_list[-1],(1,2,0)))
-plt.show()
 
 
 # %%
